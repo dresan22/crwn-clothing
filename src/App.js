fe05a22902d9f3bfc940bc8071343ae6a214a4e6
dresan22 <andresat48@hotmail.com>
2021-11-23 00:19:36 -0400

    updating our app with shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 // import "./homepage.styles.scss";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
